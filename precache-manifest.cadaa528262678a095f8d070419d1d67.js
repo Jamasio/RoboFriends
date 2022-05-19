89a239f4223200043f25829b2f1f5576409db732
Jamasio <jamasioromero@gmail.com>
2022-05-19 19:16:33 +0100

    Updates

---
+++ precache-manifest.cadaa528262678a095f8d070419d1d67.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "a18d4efbc901d52f5d4e",
+    "url": "/RoboFriends/static/css/main.e92f721d.chunk.css"
+  },
+  {
+    "revision": "a18d4efbc901d52f5d4e",
+    "url": "/RoboFriends/static/js/main.a18d4efb.chunk.js"
+  },
+  {
+    "revision": "f99921d68d7ebc942685",
+    "url": "/RoboFriends/static/js/1.f99921d6.chunk.js"
+  },
+  {
+    "revision": "a0b97605280a9c082d91",
+    "url": "/RoboFriends/static/css/2.7df856c5.chunk.css"
+  },
+  {
+    "revision": "a0b97605280a9c082d91",
+    "url": "/RoboFriends/static/js/2.a0b97605.chunk.js"
+  },
+  {
+    "revision": "4cb7496509aa3077e2b1",
+    "url": "/RoboFriends/static/js/runtime~main.4cb74965.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/RoboFriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "c27b09d2af0a5d78f56908ebbbe79681",
+    "url": "/RoboFriends/index.html"
+  }
+];
